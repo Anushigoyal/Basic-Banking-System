0aabdff9a104d3cf92be6d4bc37c8fd24b6f6317
Anushigoyal <89301263+Anushigoyal@users.noreply.github.com>
2021-08-21 20:59:06 +0530

    Add files via upload

---
+++ SparksBank-main/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body class="body">
+    <div class="nave_space">
+  <?php
+  include 'navbar.php';
+  ?>
+  </div>
+  <!-- main_Section -->
+  <div class="main_section">
+    <div class="heading">
+       <!-- Introduction section -->
+       <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3>Welcome to</h3>
+                  <h1>SPARKS FOUNDATION <br> BANK</h1>
+                </div>
+              </div>
+               </div>
+    </div>
+  </div>
+
+      
+     
+  </body>
+</html>
